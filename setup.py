196763b11026468764f17d9a5e1aad6bea4f3730
sradium <sduran@correo.unimet.edu.ve>
2023-01-05 16:37:30 -0400

    fix version

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 setup(
     name="Adafruit-PlatformDetect",
     version="3.38.0",
-    use_scm_version=True,
     setup_requires=["setuptools_scm"],
     description="Platform detection for use by libraries like Adafruit-Blinka.",
     long_description=long_description,
